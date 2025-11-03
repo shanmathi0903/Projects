4715e2ecfe556613b261a65944799f0adaaf0102
shanmathi0903 <131799427+shanmathi0903@users.noreply.github.com>
2025-11-03 10:15:25 +0530

    Create Admin.java

---
+++ BusBookingApplication/beanFolder/Admin.java
@@ -0,0 +1,12 @@
+package beanFolder;
+
+public class Admin extends User{
+	
+	public Admin(){
+		this.setName("Admin");
+		this.setPhoneNumber(9876543210L);
+		this.setEmail("admin");
+		this.setPassword("admin");
+	}
+	
+}
